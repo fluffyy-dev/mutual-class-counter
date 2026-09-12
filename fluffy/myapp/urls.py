700ffef9f54081b8a7423b3b02d65cef879f35b1
fluffy <cxkisner@gmail.com>
2026-09-12 19:03:15 +0000

    Added flowers with pixijs

--- fluffy/myapp/urls.py
+++ fluffy/myapp/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("mutuals/", views.mutuals, name="mutuals"),
     path("frame-maker/", views.frame_maker, name="frame-maker"),
+    path("flowers/", views.flowers, name="flowers"),
+    path("to-do/", views.to_do, name="to-do"),
 
     path("get-moots/", get_moots_view, name="get_moots"),
 ]
--- fluffy/myapp/views.py
+++ fluffy/myapp/views.py
@@ -13,6 +13,12 @@ def home(request):
 def og_home(request):
     return render(request, "og-home.html")
 
+def flowers(request):
+    return render(request, "flowers.html")
+
+def to_do(request):
+    return render(request, "to-do.html")
+
 def frame_maker(request):
     return render(request, "frame-maker.html")
 
